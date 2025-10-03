25450253df7500437c09fb9f985bb1658c65db15
Kesington Abdulwakeel <mr.abdulwakeel@yahoo.com>
2025-10-03 01:45:42 +0100

    addition of errors and routing to login page

--- src/app/todo/signUp.tsx
+++ src/app/todo/signUp.tsx
@@ -1,25 +1,37 @@
+"use client";
 import React from 'react'
 import { useForm, SubmitHandler } from "react-hook-form";
-import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+// import { useEffect } from 'react';
 
-interface registerData {
-    firstName: string,
-    lastName: string,
-    email: string,
-    age: number
-    password: string,
+interface RegisterData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    age: number;
+    password: string;
+    confirmPassword: string;
 }
 
 export const SignUp = () => {
+    const router = useRouter();
 
-    const onSubmit: SubmitHandler<registerData> = data => {
+    
+    const { 
+        register,
+        handleSubmit, 
+        watch,  
+        formState: { errors }, 
+        reset 
+    } = useForm<RegisterData>()
+    
+    const onSubmit: SubmitHandler<RegisterData> = data => {
         console.log(data)
-        reset()
         localStorage.setItem('regsiter', JSON.stringify(data))
+        reset()
+        router.push("/signIn")
     }
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm<registerData>()
-    
   return <div className=''>
     <h2>Registration form</h2>
 
@@ -37,6 +49,7 @@ export const SignUp = () => {
                     }) 
 
                 }/>
+                <p className="text-red-600">{errors.firstName?.message}</p>
             </div>
 
             <div>
@@ -51,6 +64,7 @@ export const SignUp = () => {
                     }) 
 
                 }/>
+                <p className="text-red-600">{errors.lastName?.message}</p>
             </div>
 
             <div>
@@ -58,14 +72,61 @@ export const SignUp = () => {
                 <input type="email" placeholder='please enter your email address' {
                     ...register("email", {
                         validate: (val) => {
-                            if (!/^[a-zA-Z]{3,100}$/.test(val)) {
-                                return "Boss, put in a real name!";
+                            if (!/^\S+@\S+\.\S+$/.test(val)) {
+                                return "Boss, put in a real email!"; 
                             }
                         }
                     }) 
 
                 }/>
+                <p className="text-red-600">{errors.email?.message}</p>
             </div>
+
+            <div>
+                <label htmlFor="age">First Name</label>
+                <input type="number" placeholder='please enter your age' {
+                    ...register("age", {
+                        validate: (val) => {
+                            if (val < 16) {
+                                return "Boss, you must be at least 16 years old!"; 
+                            }
+                        }
+                    }) 
+                }/>
+                <p className="text-red-600">{errors.age?.message}</p>
+            </div>
+
+            <div>
+                <label htmlFor="password">First Name</label>
+                <input type="password" placeholder='please enter your password' {
+                    ...register("password", {
+                        required: "Password is required",
+                        pattern: {
+                        value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/,
+                        message:
+                            "Password must be at least 8 chars, include uppercase, lowercase and a number",
+                        }
+                    }) 
+                }/>
+
+                <p className="text-red-600">{errors.password?.message}</p>
+            </div>
+
+            <div>
+                <label htmlFor="password">First Name</label>
+                <input
+                type="password"
+                placeholder="please Confirm Password"
+                {...register("confirmPassword", {
+                    required: "Please confirm your password",
+                    validate: (val) =>
+                        val === watch("password") || "Passwords do not match",
+                })}
+                />
+                <p className="text-red-600">{errors.confirmPassword?.message}</p>
+            </div>
+
+            <button type='submit'>Submit</button>
         </form>
     </div>
   </div>
